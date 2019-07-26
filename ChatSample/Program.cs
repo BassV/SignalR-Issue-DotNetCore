b9d24c91297736df271df074e3e428e083cef549
Vahagn Khorotyan <khorotyanvahagn@gmail.com>
2019-07-26 14:43:44 +0400

    Full working version

---
+++ ChatSample/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace ChatSample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false)
+                .Build();
+
+            CreateWebHostBuilder(args)
+                .UseUrls(config.GetSection("ApplicationUrl").Value)
+                .Build()
+                .Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ ChatSample/Startup.cs
@@ -0,0 +1,40 @@
+﻿using ChatSample.Hubs;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace ChatSample
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR(hubOptions =>
+            {
+                hubOptions.EnableDetailedErrors = true;
+                hubOptions.HandshakeTimeout = TimeSpan.FromSeconds(15);
+                hubOptions.ClientTimeoutInterval = TimeSpan.FromSeconds(30);
+                hubOptions.KeepAliveInterval = TimeSpan.FromSeconds(15);
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseFileServer();
+
+            app.UseSignalR(routes =>
+            {
+                routes.MapHub<ChatHub>("/hubs/chat");
+            });
+        }
+    }
+}
